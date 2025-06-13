a84403f3b58c74954102b3df9ef350bf7889fd3b
theekaguru <danielkaguru2001@gmail.com>
2025-06-13 04:23:19 +0300

    event on change

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[name , setName] = useState("")
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Welcome {name ? `,${name}` : "No name"} !</h2>
+
+
+      <input type='text'
+      value={name}
+      placeholder='Enter Your Name  '
+      style={{padding:'10px'}}
+      onChange={e=>setName(e.target.value)}
+      />
     </>
   )
 }
